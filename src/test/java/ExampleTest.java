b241e07aa4b6fc8abba310eb57fc3ae1a2df51b4
jdob <jdobies@redhat.com>
2021-02-20 13:04:34 -0500

    added pom.xml and upgraded to junit 5.7

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,7 @@
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 
 // Please delete this file prior to submitting your project.
